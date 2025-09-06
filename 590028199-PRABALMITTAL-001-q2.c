c0e119032e2e718ee9dc24006a001cebd4f2c242
prabalmittal012008 <prabalmittal012008@gmail.com>
2025-09-06 14:30:51 +0530

    q2

---
+++ 590028199-PRABALMITTAL-001-q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    printf("enter number : ");
+    scanf("%d",&x);
+    printf("enter number : ");
+    scanf("%d",&y);
+    printf("sum of numbers: %d\n",x+y);
+    printf("difference of two numbers : %d\n",x-y);
+    printf("product of two numbers : %d\n",x*y);
+    printf("quotient of two numbers : %d\n",x/y);
+    return 0;
+}
