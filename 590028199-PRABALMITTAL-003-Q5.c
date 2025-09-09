d168bfd22ce0daab89aa303c566344a27931f16e
prabalmittal012008 <prabalmittal012008@gmail.com>
2025-09-09 09:20:27 +0530

    Q5

---
+++ 590028199-PRABALMITTAL-003-Q5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    
+    fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+    
+    printf("%.2f°C is equal to %.2f°F\n", celsius, fahrenheit);
+
+    return 0;
+}
