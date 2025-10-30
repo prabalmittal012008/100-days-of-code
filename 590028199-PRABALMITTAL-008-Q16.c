5088c33d9ac0ff6fe6ecba5cd23efd9daac34d7e
prabalmittal012008 <prabalmittal012008@gmail.com>
2025-10-30 13:34:27 +0530

    q 16

---
+++ 590028199-PRABALMITTAL-008-Q16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, num3;
+    
+    printf("Enter three numbers: ");
+    scanf("%f %f %f", &num1, &num2, &num3);
+    
+    if (num1 >= num2 && num1 >= num3) {
+        printf("Largest number = %.2f\n", num1);
+    } 
+    else if (num2 >= num1 && num2 >= num3) {
+        printf("Largest number = %.2f\n", num2);
+    } 
+    else {
+        printf("Largest number = %.2f\n", num3);
+    }
+    
+    return 0;
+}
