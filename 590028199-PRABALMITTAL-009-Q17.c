f7a962aad1f5f928a11cbd78f43c8d53d1b833f0
prabalmittal012008 <prabalmittal012008@gmail.com>
2025-10-30 13:35:55 +0530

    q 17

---
+++ 590028199-PRABALMITTAL-009-Q17.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c;
+    double discriminant, root1, root2, realPart, imagPart;
+    
+    printf("Enter coefficients a, b, c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    
+    if (a == 0) {
+        printf("Error: This is not a quadratic equation (a = 0)\n");
+        return 1;
+    }
+    
+    discriminant = b * b - 4 * a * c;
+    
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        
+        printf("Roots are real and distinct:\n");
+        printf("Root 1 = %.2lf\n", root1);
+        printf("Root 2 = %.2lf\n", root2);
+    }
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        
+        printf("Roots are real and equal:\n");
+        printf("Root 1 = Root 2 = %.2lf\n", root1);
+    }
+    else {
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        
+        printf("Roots are complex and conjugate:\n");
+        printf("Root 1 = %.2lf + %.2lfi\n", realPart, imagPart);
+        printf("Root 2 = %.2lf - %.2lfi\n", realPart, imagPart);
+    }
+    
+    printf("Discriminant = %.2lf\n", discriminant);
+    
+    return 0;
+}
